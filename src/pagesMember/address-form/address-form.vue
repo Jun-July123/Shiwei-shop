8dc409c4a193e85b3b2d972a8763592d807c0c5c
Jun <Jun@123456>
2026-06-15 21:34:32 +0800

    37-6-收货地址（地址表单校验）

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -58,42 +58,75 @@ const onSwitchChange: UniHelper.SwitchOnChange = (e: any) => {
   form.value.isDefault = e.detail.value ? 1 : 0
 }
 
+// 37-6.1 定义地址表单校验规则
+const rules = {
+  receiver: {
+    rules: [{ required: true, errorMessage: '请输入收货人姓名' }],
+  },
+  contact: {
+    rules: [
+      { required: true, errorMessage: '请输入联系方式' },
+      { pattern: /^1[3-9]\d{9}$/, errorMessage: '手机号格式不正确' },
+    ],
+  },
+  fullLocation: {
+    rules: [{ required: true, errorMessage: '请选择所在地区' }],
+  },
+  address: {
+    rules: [{ required: true, errorMessage: '请选择详细地址' }],
+  },
+}
+// 37-6.5 获取表单实例formRef
+const formRef = ref<UniHelper.UniFormsInstance>()
+
 const onSubmit = async () => {
-  if (query.id) {
-    // 37-5.2 提交表单，存在id，调用修改地址接口，传递地址id和表单数据
-    await putMemberAddressByIdAPI(query.id, form.value)
-  } else {
-    // 37-2.8.2 处理提交事件,将收集到的表单数据提交至后端新增地址接口
-    await postMemberAddressAPI(form.value)
+  // 37-6.6 提交表单时，先进行表单校验,校验通过后提交表单,否则提示用户填写完整信息
+  try {
+    await formRef.value?.validate?.()
+    if (query.id) {
+      // 37-5.2 提交表单，存在id，调用修改地址接口，传递地址id和表单数据
+      await putMemberAddressByIdAPI(query.id, form.value)
+    } else {
+      // 37-2.8.2 处理提交事件,将收集到的表单数据提交至后端新增地址接口
+      await postMemberAddressAPI(form.value)
+    }
+    // 37-2.8.3 提交成功,显示成功提示,并返回上一页
+    uni.showToast({
+      // 37-5.3 提交成功，存在id显示修改成功，否则提交成功
+      title: query.id ? '修改地址成功' : '新增地址成功',
+      icon: 'success',
+    })
+    setTimeout(() => {
+      uni.navigateBack()
+    }, 1000)
+  } catch (err) {
+    uni.showToast({
+      title: '请填写完整信息',
+      icon: 'none',
+    })
   }
-  // 37-2.8.3 提交成功,显示成功提示,并返回上一页
-  uni.showToast({
-    // 37-5.3 提交成功，存在id显示修改成功，否则提交成功
-    title: query.id ? '修改地址成功' : '新增地址成功',
-    icon: 'success',
-  })
-  setTimeout(() => {
-    uni.navigateBack()
-  }, 1000)
 }
 </script>
 
 <template>
   <view class="content">
-    <form>
+    <!-- 37-6.2 修改form表单结构为uni-forms,绑定校验规则rules,绑定需校验的表单数据form -->
+    <!-- 37-6.4 ref绑定地址表单 -->
+    <uni-forms :rules="rules" :model="form" ref="formRef">
       <!-- 表单内容 -->
       <!-- 37-2.5 绑定收集表单数据 -->
-      <view class="form-item">
+      <!-- 37-6.3 修改view表单项结构为uni-forms-item,添加name属性，校验rules.receiver -->
+      <uni-forms-item class="form-item" name="receiver">
         <text class="label">收货人</text>
         <!-- 37-2.5.1 v-model 绑定收集收货人姓名 -->
         <input v-model="form.receiver" class="input" placeholder="请填写收货人姓名" />
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item class="form-item" name="contact">
         <text class="label">手机号码</text>
         <!-- 37-2.5.2 v-model 绑定收集收货人手机号码 -->
         <input v-model="form.contact" class="input" placeholder="请填写收货人手机号码" />
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item class="form-item" name="fullLocation">
         <text class="label">所在地区</text>
         <!-- 37-2.6.1 @change事件获取所选择的地区 -->
         <!-- 37-2.6.3 :value 绑定收集所选择的地区 -->
@@ -106,13 +139,13 @@ const onSubmit = async () => {
           <view v-if="form.fullLocation">{{ form.fullLocation }}</view>
           <view v-else class="placeholder">请选择省/市/区(县)</view>
         </picker>
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item class="form-item">
         <text class="label">详细地址</text>
         <!-- 37-2.5.3 v-model 绑定收集详细地址 -->
         <input v-model="form.address" class="input" placeholder="街道、楼牌号等信息" />
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item class="form-item">
         <label class="label">设为默认地址</label>
         <!-- 37-2.7.1 添加@change是否设置为默认地址 -->
         <!-- 37-2.7.3 :checked 绑定收集是否默认地址的状态,1为是 选中设置高亮,0为否 -->
@@ -122,8 +155,8 @@ const onSubmit = async () => {
           class="switch"
           color="#27ba9b"
         />
-      </view>
-    </form>
+      </uni-forms-item>
+    </uni-forms>
   </view>
   <!-- 提交按钮 -->
   <!-- 37-2.8.1 保存按钮，注册提交事件 -->
